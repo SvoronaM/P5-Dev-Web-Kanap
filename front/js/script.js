7f7dcdfa14bf07f24ee36f26ab0fd5e97187b375
SvoronaM <sergeymigal@gmail.com>
2022-08-01 21:15:07 +0200

    CreatJs

--- front/js/script.js
+++ front/js/script.js
@@ -8,20 +8,20 @@ fetch('http://localhost:3000/api/products')
     })
 
     .catch(_error => {
-        alert('Oops ! Le serveur ne répond pas, suivez les instructions dans le READ.me.');
-    });
+        alert('Oops ! Le serveur ne répond pas, suivez les instructions dans le READ.me.')
+    })
 
 
 function showProducts(data) {
 
     for (product of data) {
 
-        const itemCard = document.getElementById('items');
+        const itemCard = document.getElementById('items')
 
         itemCard.innerHTML +=`<a href="./product.html?id=${product._id}">
         <article><img src="${product.imageUrl}" alt="${product.altTxt}">
           <h3 class="productName">${product.name}</h3>
-          <p class="productDescription">${product.description}</p></article></a>`;
+          <p class="productDescription">${product.description}</p></article></a>`
     }
 }
 
