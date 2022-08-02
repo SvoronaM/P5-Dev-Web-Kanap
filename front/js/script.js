16dec407afae2d127864d1aecba0dbde262cf362
SvoronaM <sergeymigal@gmail.com>
2022-08-02 21:24:44 +0200

    script.js

--- front/js/script.js
+++ front/js/script.js
@@ -11,19 +11,37 @@ fetch('http://localhost:3000/api/products')
         alert('Le serveur ne répond pas, suivez les instructions dans le READ.me.')
     })
 
-//Affichage tous les produit
+// Affichage des produits
 function showProducts(data) {
-//product
-    for (product of data) {
-//élément items dans index.html
-        const itemCard = document.getElementById('items')
-//ajouter tous les éléments
-        itemCard.innerHTML +=`<a href="./product.html?id=${product._id}">
-        <article>
-        <img src="${product.imageUrl}" alt="${product.altTxt}">
-          <h3 class="productName">${product.name}</h3>
-          <p class="productDescription">${product.description}</p>
-        </article></a>`
+    for (let i = 0; i < data.length; i++){
+        let product = data[i]
+        let allPanels = document.querySelector('.items')
+
+        // insertion du lien de chaque canapés
+        let createLinkPanel = document.createElement('a')
+        createLinkPanel.setAttribute('href', "./product.html?id=" + product._id)
+        allPanels.appendChild(createLinkPanel)
+
+        // insertion des articles
+        let createArticle = document.createElement('article')
+        createLinkPanel.appendChild(createArticle)
+
+        // insertion des images
+        let createPict = document.createElement('img')
+        createPict.setAttribute('src', product.imageUrl)
+        createPict.setAttribute('alt', product.altTxt)
+        createArticle.appendChild(createPict)
+
+        // insertion des noms dans h3
+        let createH3 = document.createElement('h3')
+        createH3.className = 'productName'
+        createH3.textContent = product.name
+        createArticle.appendChild(createH3)
+
+        // insertion des descriptions dans p
+        let createP = document.createElement('p')
+        createP.className = 'productDescription'
+        createP.textContent = product.description
+        createArticle.appendChild(createP)
     }
 }
-
