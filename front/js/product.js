40523f23de950962c3cfd00b91f65c316970aae4
SvoronaM <sergeymigal@gmail.com>
2022-08-07 12:34:44 +0200

    product.js

--- front/js/product.js
+++ front/js/product.js
@@ -1,53 +1,53 @@
-let string = window.location.href;
-let url = new URL(string);
-let idURL = url.searchParams.get("id");
+let string = window.location.href
+let url = new URL(string)
+let idURL = url.searchParams.get("id")
 // Appel API avec l'id du produit
-let productUnit = "";
+let productUnit = ""
 let requestURL = "http://localhost:3000/api/products/" + idURL
 fetch(requestURL)
     .then(response => response.json())
     .then(async function (resultatAPI) {
-        productUnit = await resultatAPI;
-        showProduct(productUnit);
+        productUnit = await resultatAPI
+        showProduct(productUnit)
     })
     // ajoute un message au cas où le serveur ne répond pas
-    .catch(error => alert('Le serveur ne répond pas, suivez les instructions dans le READ.me.' + error));
+    .catch(error => alert('Le serveur ne répond pas, suivez les instructions dans le READ.me.' + error))
         // je modifie le contenu de chaque variable avec les bonnes données :
         function showProduct(data) {
-            document.title = data.name;
-            let panelImages = document.querySelector('.item__img');
+            document.title = data.name
+            let panelImages = document.querySelector('.item__img')
             // insertion image du canapé
-            let createPict = document.createElement('img');
-            createPict.setAttribute('src', data.imageUrl);
-            createPict.setAttribute('alt', data.altTxt);
-            panelImages.appendChild(createPict);
+            let createPict = document.createElement('img')
+            createPict.setAttribute('src', data.imageUrl)
+            createPict.setAttribute('alt', data.altTxt)
+            panelImages.appendChild(createPict)
             // insertion du nom du canapé
-            let panelH1 = document.querySelector('#title');
-            panelH1.textContent = data.name;
+            let panelH1 = document.querySelector('#title')
+            panelH1.textContent = data.name
             // insertion du prix du canapé
-            let panelPrice = document.querySelector('#price');
-            panelPrice.textContent =data.price;
+            let panelPrice = document.querySelector('#price')
+            panelPrice.textContent =data.price
             // insertion du choix du canapé
-            let panelDescription = document.querySelector('#description');
-            panelDescription.textContent = data.description;
+            let panelDescription = document.querySelector('#description')
+            panelDescription.textContent = data.description
             // récupération de #colors
-            let panelOption = document.querySelector('#colors');
+            let panelOption = document.querySelector('#colors')
             // insertion du tableau des couleurs dans une variable
-            let colors = data.colors;
+            let colors = data.colors
             // parcour du tableau des couleurs et insertion de celles-ci dans choix
             for (let i = 0; i < colors.length; i++){
-                let colorProduct = colors[i];
-                let createOption = document.createElement('option');
-                createOption.setAttribute('value', colorProduct);
-                createOption.textContent = colorProduct;
-                panelOption.appendChild(createOption);
+                let colorProduct = colors[i]
+                let createOption = document.createElement('option')
+                createOption.setAttribute('value', colorProduct)
+                createOption.textContent = colorProduct
+                panelOption.appendChild(createOption)
             }
         }
 //     RECUPERATION DES DONNEES PAR RAPPORT AU CHOIX DE L'UTILISATEUR
-const selectQuantity = document.getElementById('quantity');
-const selectColors = document.getElementById('colors');
+const selectQuantity = document.getElementById('quantity')
+const selectColors = document.getElementById('colors')
 // configuration un eventListener quand l'utilisateur clique sur ajouter au panier
-const addToCart = document.getElementById('addToCart');
+const addToCart = document.getElementById('addToCart')
 addToCart.addEventListener('click', (event) => {
     event.preventDefault();
 
@@ -59,42 +59,42 @@ addToCart.addEventListener('click', (event) => {
         price: price.textContent,
         color: selectColors.value,
         quantity: selectQuantity.value,
-    };
+    }
     // je déclare une variable productInLocalStorage
-    let productInLocalStorage =  JSON.parse(localStorage.getItem('product'));
+    let productInLocalStorage =  JSON.parse(localStorage.getItem('product'))
     // Ajoute les produits sélectionnés dans le localStorage
     const addProductLocalStorage = () => {
         // récupèration de la sélection de l'utilisateur dans le tableau de l'objet :
-        productInLocalStorage.push(selection);
+        productInLocalStorage.push(selection)
         // stockage des données récupérées dans le localStorage :
-        localStorage.setItem('product', JSON.stringify(productInLocalStorage));
+        localStorage.setItem('product', JSON.stringify(productInLocalStorage))
     }
     // création d'une boîte de dialogue pour confirmer l'ajout au panier
     let addConfirm = () => {
-        alert('Le produit a bien été ajouté au panier');
+        alert('Le produit a bien été ajouté au panier')
     }
-    let update = false;
+    let update = false
     // s'il y a des produits enregistrés dans le localStorage
     if (productInLocalStorage) {
         // verification que le produit ne soit pas deja dans le localstorage/panier
         productInLocalStorage.forEach (function (productOk, key) {
             if (productOk.id == newID && productOk.color == selectColors.value) {
-                productInLocalStorage[key].quantity = parseInt(productOk.quantity) + parseInt(selectQuantity.value);
-                localStorage.setItem('product', JSON.stringify(productInLocalStorage));
-                update = true;
-                addConfirm();
+                productInLocalStorage[key].quantity = parseInt(productOk.quantity) + parseInt(selectQuantity.value)
+                localStorage.setItem('product', JSON.stringify(productInLocalStorage))
+                update = true
+                addConfirm()
             }
         });
         if (!update) {
-            addProductLocalStorage();
-            addConfirm();
+            addProductLocalStorage()
+            addConfirm()
         }
     }
     // s'il n'y a aucun produit enregistré dans le localStorage
     else {
         // création un tableau avec les éléments choisi par l'utilisateur
-        productInLocalStorage = [];
-        addProductLocalStorage();
-        addConfirm();
+        productInLocalStorage = []
+        addProductLocalStorage()
+        addConfirm()
     }
 });
