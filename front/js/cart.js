d8d9932767d2272376bf0ba93c1d21377b50637c
SvoronaM <sergeymigal@gmail.com>
2022-08-19 17:03:58 +0200

    product.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,3 +1,16 @@
 // RECUPERER LES PRODUITS STOCKES DANS LE LOCALSTORAGE   //
-let products = [];
-let productInLocalStorage = JSON.parse(localStorage.getItem('product'));
+let basketStr = localStorage.getItem('basket');
+let basket = JSON.parse(basketStr);
+
+// Récupération de l'élement "cart__items"
+let cartPanel = document.querySelector('#cart__items');
+
+// Affichage des produits dans la page panier (avec les prix en fetch)
+function showProductBasket(produit) {
+    // AFFICHAGE DU/DES PRODUIT(S) PANIER
+    // insertion des articles
+    let creatArticle = document.creatElement('article');
+    creatArticle.className = 'cart__item';
+    creatArticle.setAttribute('data-id', produit.id);
+    creatArticle.setAttribute('data-color', produit.color);
+    cartPanel.appendChild(creatArticle);
