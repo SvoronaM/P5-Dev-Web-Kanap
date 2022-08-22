6643d74d822d910c425c5a3aa6136c3b9372a2ca
SvoronaM <sergeymigal@gmail.com>
2022-08-22 20:59:47 +0200

    cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -5,13 +5,13 @@ let basket = JSON.parse(basketStr);
 let cartPanel = document.querySelector('#cart__items');
 // Affichage des produits dans la page panier (avec les prix en fetch)
 function showProductBasket(produit) {
-    // insertion des articles
+    // Insertion des articles
     let creatArticle = document.creatElement('article');
     creatArticle.className = 'cart__item';
     creatArticle.setAttribute('data-id', produit.id);
     creatArticle.setAttribute('data-color', produit.color);
     cartPanel.appendChild(creatArticle);
-    // insertion div de l'img
+    // Insertion div de l'img
     let creatDivIMG = document.creatElement('div');
     creatDivIMG.className = 'cart__item__img';
     creatArticle.appendChild(creatDivIMG);
