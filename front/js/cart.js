98261a0c487d5209fecb57000e87d73fda856c89
SvoronaM <sergeymigal@gmail.com>
2022-08-23 10:21:43 +0200

    cart.js

--- front/js/cart.js
+++ front/js/cart.js
@@ -6,34 +6,34 @@ let cartPanel = document.querySelector('#cart__items');
 // Affichage des produits dans la page panier (avec les prix en fetch)
 function showProductBasket(produit) {
     // Insertion des articles
-    let creatArticle = document.creatElement('article');
+    let creatArticle = document.createElement('article');
     creatArticle.className = 'cart__item';
     creatArticle.setAttribute('data-id', produit.id);
     creatArticle.setAttribute('data-color', produit.color);
     cartPanel.appendChild(creatArticle);
     // Insertion div de l'img
-    let creatDivIMG = document.creatElement('div');
+    let creatDivIMG = document.createElement('div');
     creatDivIMG.className = 'cart__item__img';
     creatArticle.appendChild(creatDivIMG);
     // Insertion des images
-    let creatPict = document.creatElement('img');
+    let creatPict = document.createElement('img');
     creatPict.setAttribute('src', produit.img);
     creatPict.setAttribute('alt', "Photographie d'un canapé");
     creatDivIMG.appendChild(creatPict);
     // Insertion div content
-    let creatDivContDes = document.creatElement('div');
+    let creatDivContDes = document.createElement('div');
     creatDivContDes.className = 'cart__item__content';
     creatArticle.appendChild(creatDivContDes);
     // Insertion div description
-    let creatDivDes = document.creatElement('div');
+    let creatDivDes = document.createElement('div');
     creatDivDes.className = 'cart__item__content__description';
     creatDivContDes.appendChild(creatDivDes);
     // Insertion H2
-    let creatH2 = document.creatElement('h2');
+    let creatH2 = document.createElement('h2');
     creatH2.textContent = produit.name;
     creatDivDes.appendChild(creatH2);
     // Insertion P color
-    let creatpColor = document.creatElement('p');
+    let creatpColor = document.createElement('p');
     creatpColor.textContent = "Couleur : " + produit.color;
     creatDivDes.appendChild(creatpColor);
     // Recupération du prix en utilisant l'id du produit
@@ -43,25 +43,25 @@ function showProductBasket(produit) {
         .then(async function (resultatAPI) {
             productUnit = await resultatAPI;
             // insertion P price
-            let creatpPrice = document.creatElement('p');
+            let creatpPrice = document.createElement('p');
             creatpPrice.textContent = "Prix : " + productUnit.price + " € / canapé";
             creatDivDes.appendChild(creatpPrice);
         })
         .catch(error => alert("Erreur : " + error));
     // Insertion div content settings
-    let creatDivContSet = document.creatElement('div');
+    let creatDivContSet = document.createElement('div');
     creatDivContSet.className = 'cart__item__content__settings';
     creatDivContDes.appendChild(creatDivContSet);
     // Insertion div settings quantity
-    let creatDivContSetQuantity = document.creatElement('div');
+    let creatDivContSetQuantity = document.createElement('div');
     creatDivContSetQuantity.className = 'cart__item__content__settings__quantity';
     creatDivContSet.appendChild(creatDivContSetQuantity);
     // Insertion P quantity
-    let creatpQuantity = document.creatElement('p');
+    let creatpQuantity = document.createElement('p');
     creatpQuantity.textContent = "Qté :";
     creatDivContSetQuantity.appendChild(creatpQuantity);
     // Insertion input quantity
-    let creatInputQuantity = document.creatElement('input');
+    let creatInputQuantity = document.createElement('input');
     creatInputQuantity.className = 'itemQuantity';
     creatInputQuantity.setAttribute('type', 'number');
     creatInputQuantity.setAttribute('name', 'itemQuantity');
@@ -70,13 +70,13 @@ function showProductBasket(produit) {
     creatInputQuantity.setAttribute('value', produit.quantity);
     creatDivContSetQuantity.appendChild(creatInputQuantity);
     // Insertion div settings delete
-    let creatDivContSetDel = document.creatElement('div');
+    let creatDivContSetDel = document.createElement('div');
     creatDivContSetDel.className = 'cart__item__content__settings__delete';
     creatDivContSet.appendChild(creatDivContSetDel);
     // Insertion P delete item
-    let creatpDelete = document.creatElement('p');
+    let creatpDelete = document.createElement('p');
     creatpDelete.className = 'deleteItem';
     creatpDelete.textContent = "Supprimer";
     creatDivContSetDel.appendChild(creatpDelete);
 }
-
+           showProductBasket(produit.color)
