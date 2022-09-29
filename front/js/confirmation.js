aaca9eca49eb3aa017aa0eaad1a65832ef53c503
SvoronaM <sergeymigal@gmail.com>
2022-09-29 13:57:41 +0200

    confirmation

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,8 +1,10 @@
 //Récupération du numéro de commande dans l'URL
-let str = window.location.href;
-let url = new URL(str);
-let idOrderURL = url.searchParams.get("orderId");
+// window.location.href renvoie le href (URL) de la page en cours
+let str = window.location.href
+let url = new URL(str)
+// url.searchParams.get renvoie la première valeur associée au paramètre de recherche donné(id).
+let idOrderURL = url.searchParams.get("orderId")
 
-//Affichage du numéro de commande
-let orderIdNumberElt = document.querySelector('#orderId');
-orderIdNumberElt.innerText = idOrderURL;
+//Affichage du numéro de commande, retour id avec querySelector
+let orderIdNumberElt = document.querySelector('#orderId')
+orderIdNumberElt.innerText = idOrderURL
