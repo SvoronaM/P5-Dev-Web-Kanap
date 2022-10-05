6c7909698dca84fb097b31856c4879ba2f5046d2
SvoronaM <sergeymigal@gmail.com>
2022-10-05 15:09:55 +0200

    cart.js

--- front/js/script.js
+++ front/js/script.js
@@ -16,30 +16,25 @@ function showProducts(data) {
     for (let i = 0; i < data.length; i++){
         let product = data[i]
         let allPanels = document.querySelector('.items')
-
-        // insertion du lien de chaque canapés
         let createLinkPanel = document.createElement('a')
+        let createArticle = document.createElement('article')
+        let createPict = document.createElement('img')
+        let createH3 = document.createElement('h3')
+        let createP = document.createElement('p')
+        // insertion du lien de chaque canapés
         createLinkPanel.setAttribute('href', "./product.html?id=" + product._id)
         allPanels.appendChild(createLinkPanel)
-
         // insertion des articles
-        let createArticle = document.createElement('article')
         createLinkPanel.appendChild(createArticle)
-
         // insertion des images
-        let createPict = document.createElement('img')
         createPict.setAttribute('src', product.imageUrl)
         createPict.setAttribute('alt', product.altTxt)
         createArticle.appendChild(createPict)
-
         // insertion des noms dans h3
-        let createH3 = document.createElement('h3')
         createH3.className = 'productName'
         createH3.textContent = product.name
         createArticle.appendChild(createH3)
-
         // insertion des descriptions dans p
-        let createP = document.createElement('p')
         createP.className = 'productDescription'
         createP.textContent = product.description
         createArticle.appendChild(createP)
