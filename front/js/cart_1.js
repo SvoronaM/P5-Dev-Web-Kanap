49b5f35c24ca136bce47f2dd77998a967adfd52b
SvoronaM <sergeymigal@gmail.com>
2022-10-06 16:06:01 +0200

    cart.js

--- front/js/cart_1.js
+++ front/js/cart_1.js
@@ -15,8 +15,6 @@ function getCart() {
         return JSON.parse(cart);
     }
 }
-
-
 // Crée une fonction pour ajouter les produits a cart
 function addToCart(product) {
     // Récupération de la carte
@@ -33,133 +31,250 @@ function addToCart(product) {
     // Enregistre la carte modifié avec la fonction saveCart
     saveCart(cart);
 }
+// récupération de la cart avec la fonction getCart
+let cart = getCart();
 
-// Crée une fonction pour supprimer des produits de la cart
-function removeFromCart(product) {
-    let cart = getCart();
-    // Filtrer la carte pour garder uniquement les produits avec une id différentes ou les produits avec une différentes couleur et la même id
-    cart = cart.filter((p) => p.id != product.id || p.color != product.color);
-    saveCart(cart);
+// Récupération des éléments dans une constante
+const cartList = document.getElementById("cart__items");
+const cartHeading = document.querySelector("h1");
+const totalDisplay = document.querySelector(".cart__price p");
+const orderForm = document.querySelector(".cart__order");
+
+
+// Récupération des éléments qui afficheront le prix total et la quantité de produit dans la cart
+let totalProductsQuantity = document.getElementById("totalQuantity");
+let totalPrice = document.getElementById("totalPrice");
+// Initialisation de totalCartPrice qui sera mis à jour ultérieurement avec le résultat renvoyé par la fonction getTotalPrice
+let totalCartPrice = 0;
+
+
+for (let product of cart) {
+    // Pour chaque produit, récupération de l'identifiant, de la couleur, de la quantité et du nom
+    let productId = product.id;
+    let productColor = product.color;
+    let productQuantity = product.quantity;
+    let productName = product.name;
+    fetch(`http://localhost:3000/api/products/${productId}`)
+
+        .then((response) => response.json())
+        // Définir la réponse de l'API en tant que productDetails et définir l'action à exécuter pour chaque produit du panier
+        .then((productDetails) => {
+
+            // Insertion de l'élément article, définition des attributs, ajout de la classe cart__item et définition en tant qu'enfant de l'élément cartList
+            let productArticle = document.createElement("article");
+            productArticle.classList.add("cart__item");
+            productArticle.setAttribute("data-id", productId);
+            productArticle.setAttribute("data-color", productColor);
+            cartList.appendChild(productArticle);
+
+            // Insertion de l'élément productImgContainer
+            let productImgContainer = document.createElement("div");
+            productImgContainer.classList.add("cart__item__img");
+            productArticle.appendChild(productImgContainer);
+
+            // Insertion de l'élément productImg
+            let productImg = document.createElement("img");
+            productImg.setAttribute("src", productDetails.imageUrl);
+            productImg.setAttribute("alt", productDetails.altTxt);
+            productImgContainer.appendChild(productImg);
+
+            // Insertion de l'élément productContent
+            let productContent = document.createElement("div");
+            productContent.classList.add("cart__item__content");
+            productArticle.appendChild(productContent);
+
+            // Insertion de l'élément productContentDescription
+            let productContentDescription = document.createElement("div");
+            productContentDescription.classList.add("cart__item__content__description");
+            productContent.appendChild(productContentDescription);
+
+            // Insertion de l'élément productName
+            let productName = document.createElement("h2");
+            productName.textContent = productDetails.name;
+            productContentDescription.appendChild(productName);
+
+            // Insertion de l'élément poductColorPicked
+            let productColorPicked = document.createElement("p");
+            productColorPicked.textContent = productColor;
+            productContentDescription.appendChild(productColorPicked);
+
+            // Insertion de l'élément productPrice (insertion de la valeur renvoyée par l'API, suivie du symbole €)
+            let productPrice = document.createElement("p");
+            productPrice.textContent = `${productDetails.price} €`;
+            productContentDescription.appendChild(productPrice);
+
+            // Insertion de l'élément productContentSettings
+            let productContentSettings = document.createElement("div");
+            productContentSettings.classList.add("cart__item__content__settings");
+            productContent.appendChild(productContentSettings);
+
+            // Insertion de l'élément productQuantitySettings
+            let productQuantitySettings = document.createElement("div");
+            productQuantitySettings.classList.add("cart__item__content__settings__quantity");
+            productContentSettings.appendChild(productQuantitySettings);
+
+            // Insertion de l'élément productQuantityPickedLabel
+            let productQuantityPickedLabel = document.createElement("p");
+            productQuantityPickedLabel.textContent = "Quantité : ";
+            productQuantitySettings.appendChild(productQuantityPickedLabel);
+
+            // Insertion de l'élément productQuantityPicked, ajout d'attributs, insertion de la quantité sélectionnée par l'utilisateur à l'étape précédente
+            let productQuantityPicked = document.createElement("input");
+            productQuantityPicked.setAttribute("type", "number");
+            productQuantityPicked.setAttribute("name", "itemQuantity");
+            productQuantityPicked.setAttribute("min", 1);
+            productQuantityPicked.setAttribute("max", 100);
+            productQuantityPicked.setAttribute("value", productQuantity);
+            productQuantityPicked.classList.add("itemQuantity");
+            productQuantitySettings.appendChild(productQuantityPicked);
+
+            // Insertion de l'élément productDelete
+            let productDelete = document.createElement("div");
+            productDelete.classList.add("cart__item__content__settings__delete");
+            productContentSettings.appendChild(productDelete);
+
+            // Insertion de l'élément productDeleteButton
+            let productDeleteButton = document.createElement("p");
+            productDeleteButton.classList.add("deleteItem");
+            productDeleteButton.textContent = "Supprimer";
+            productDelete.appendChild(productDeleteButton);
+
+            // Ajout du listener pour exécuter l'action removeFromCart lorsque le bouton de suppression est cliqué
+            productDeleteButton.addEventListener("click", function () {
+                removeFromCart(product);
+                alert("L'article a été retiré de votre panier");
+                document.location.reload();
+            });
+
+
+            // Utilisation de la fonction getNumberOfProduct pour afficher la quantité totale sur la page
+            totalProductsQuantity.textContent = getNumberOfProducts();
+
+            // Utilisation de la fonction getTotalPrice pour afficher le prix total sur la page
+            totalPrice.textContent = getTotalPrice(productDetails, productQuantity);
+
+            // Définition de la quantité actuelle prélevée comme ancienne quantité pour une utilisation ultérieure lors d'un événement de modification
+            let oldQuantity = Number(productQuantityPicked.value);
+
+            // ajout Eventlistener de quantité et définir l'action à exécuter
+            productQuantityPicked.addEventListener("change", () => {
+
+                // Définition de la nouvelle quantité de produit à l'aide de la fonction modifyQuantity - saveCart() lors de cette étape avec la fonction modifyQuantity
+                productQuantity = modifyQuantity(product,Number(productQuantityPicked.value));
+
+                // Utilisation de la fonction modifyTotalPrice pour calculer le nouveau prix de chaque article, en fonction de la variation de quantité
+                totalPrice.textContent = modifyTotalPrice(productDetails,oldQuantity,Number(productQuantityPicked.value));
+
+                // Définition de la (new) quantité actuelle prélevée comme ancienne quantité pour une utilisation ultérieure lors d'un événement de modification
+                oldQuantity = Number(productQuantityPicked.value);
+
+                // Récupération de la quantité totale dans le panier à l'aide de la fonction getNumberOfProduct
+                totalProductsQuantity.textContent = getNumberOfProducts();
+            });
+        })
+        // Si la demande à l'API a échoué, création d'un message pour chaque produit pour informer l'utilisateur que quelque chose s'est mal passé
+        .catch((error) => {
+            console.log("Erreur dans le chargement du panier" + error);
+            let cartErrorMessage = document.createElement("h2");
+            cartErrorMessage.textContent = `L'article ${productName} de couleur ${productColor} que vous avez sélectionné semble inaccessible pour le moment.`;
+            cartErrorMessage.style.textAlign = "center";
+            cartErrorMessage.style.padding = "15px";
+            cartList.appendChild(cartErrorMessage);
+            // Modification du texte dans l'élément des totaux et suppression du formulaire de commande de la page
+            totalDisplay.textContent = "Il est impossible de procéder à la commande pour le moment. Nous vous invitons à réessayer ultérieurement";
+            totalDisplay.style.textAlign = "center";
+            orderForm.style.display = "none";
+        });
 }
 
-// Crée une fonction pour supprimer la cart quand la commande est passé
-function clearCart() {
-    let cart = getCart()
-    for (let product of cart) {
-        removeFromCart(product);
-    }
-}
 
-// Crée une fonction pour récuperer le prix total des produits dans la cart
-function getNumberOfProducts() {
-    let cart = getCart();
-    // Définie numberOfProduct = 0
-    let numberOfProduct = 0;
-    // Parcoure chaque produit
-    for (let product of cart) {
-        // Pour chaque produit dans la cart, ajoute la quantité du produit a numberOfProduct
-        numberOfProduct += product.quantity;
-    }
-    // Retourne le nombre total de produit
-    return numberOfProduct;
-}
-
-// Crée une fonction pour avoir le prix total de la cart
-function getTotalPrice(product, quantity) {
-    // Pour chaque produit dans la carte, ajoute le prix total a totalCartPrice (product.price * quantité)
-    totalCartPrice += product.price * quantity;
-    // Retourne le prix total de la carte
-    return totalCartPrice;
-}
-
-// Crée une fonction pour modifié la quantité d'un produit
-function modifyQuantity(product, quantity) {
-
-    let cart = getCart();
-
-    // vérifie si un produit avec le meme id et la même couleur sont déja dans la carte
-    let productFound = cart.find((p) => p.id == product.id && p.color == product.color);
-
-    // Si oui, ajuste la nouvelle quantité
-    if (productFound != undefined) {
-        productFound.quantity = quantity;
-
-        // si la nouvelle quantité <= 0, supprime le produit de la carte et recharge la page
-        if(quantity <= 0){
-            document.location.reload();
-            removeFromCart(productFound);
-            alert("L'article a été retiré de votre panier");
-
-            // Si la nouvelle quantité > 100, envoi un message d'alert et recharge la page
-        }else if(quantity > 100){
-            document.location.reload();
-            alert("La quantité est limitée à 100 pièces");
-        }else{
-
-            saveCart(cart);
-        }
-    }
+// Modification de la structure lorsque que la Cart est vide
+// Si le panier est vide : modification de l'en-tête, du texte dans l'élément totaux et suppression du bon de commande de la page
+if (cart.length === 0) {
+    cartHeading.textContent = "Votre panier est vide";
+    totalDisplay.innerHTML ='<a href="./index.html">Consulter notre catalogue</a>';
+    totalDisplay.style.textAlign = "center";
+    orderForm.style.display = "none";
 }
 
 
-// Crée une fonction pour modifié le prix total
-function modifyTotalPrice(product, oldQuantity, newQuantity) {
+// Récupération des éléments du formulaire et du bouton d'envoi
+let form = document.querySelector(".cart__order__form");
+let submitButton = document.querySelector("#order");
 
-    // Si la nouvelle quantité est supérieur a l'ancienne, ajoute et retourne le prix total
-    if (newQuantity > oldQuantity) {
-        totalCartPrice += product.price * (newQuantity - oldQuantity);
-        return totalCartPrice;
-        // Si la nouvelle quantité est inférieur a l'ancienne, enleve et retourne le prix total
-    } else if (newQuantity < oldQuantity) {
-        totalCartPrice -= product.price * (oldQuantity - newQuantity);
-        return totalCartPrice;
-    }
+// Ajout d'attributs de modèle et d'espace réservé à chaque entrée - EventListener pour afficher un message (succès ou erreur) dans l'élément sous chaque entrée
+form.firstName.setAttribute("pattern", "[a-z A-Z-']{2,50}");
+form.firstName.setAttribute("placeholder", "Kevin");
+form.firstName.addEventListener("input", () => {
+    textValidity(form.firstName);
+});
+form.lastName.setAttribute("pattern", "[a-z A-Z-']{2,50}");
+form.lastName.setAttribute("placeholder", "Durant");
+form.lastName.addEventListener("input", () => {
+    textValidity(form.lastName);
+});
+form.address.setAttribute("pattern", "[a-zA-Z 0-9'-]{2,50}");
+form.address.setAttribute("placeholder", "39 boulevard Emmanuel Rouquier");
+form.address.addEventListener("input", () => {
+    adressValidity(form.address);
+});
+form.city.setAttribute("pattern", "[0-9]{5}[a-zA-Zéèôöîïûùü' -]{2,50}");
+form.city.setAttribute("placeholder", "06130 Grasse");
+form.city.addEventListener("input", () => {
+    cityValidity(form.city);
+});
+form.email.setAttribute("placeholder", "exemple@mail.com");
+form.email.addEventListener("input", () => {
+    emailValidity(form.email);
+});
+
+// Initialisation du tableau de produits qui sera envoyé à l'API
+let products = [];
+
+// Pour chaque produit du panier, pousser le productId dans le tableau products
+for (let product of cart) {
+    products.push(product.id);
 }
 
-
-
-// Crée une fonction pour vérifier la validité des text inputs (firstName, LastName) et affiche un message (success or error)
-function textValidity(input) {
-    let nameRegExp = /^[a-zéèôöîïûùü' -]{2,50}$/gi;
-    let test = nameRegExp.test(input.value);
-    if (test == true) {
-        input.nextElementSibling.textContent = "Champ valide";
-    } else {
-        input.nextElementSibling.textContent = "Vous ne pouvez utiliser que des lettres, espaces, - et ' ";
-    }
-}
-
-// Crée une fonction pour vérifier la validité des text/number inputs (City) et affiche un message ( succes or error)
-function cityValidity(input) {
-    let cityRegExp = /^[0-9]{5}[a-zéèôöîïûùü' -]{2,50}$/gi;
-    let test = cityRegExp.test(input.value);
-    if (test == true) {
-        input.nextElementSibling.textContent = "Champ valide";
-    } else {
-        input.nextElementSibling.textContent = "Veuillez respecter le format CODE POSTAL (5 CHIFFRES) suivi du nom de la VILLE. Exemple : 06130 Grasse";
-    }
-}
-
-
-// Crée une fonction pour vérifier la validité des text/number input (adress) et affiche un message ( succes or error)
-function adressValidity(input) {
-    let adressRegExp = /^[a-z0-9éèôöîïûùü' -]{2,50}$/gi;
-    let test = adressRegExp.test(input.value);
-    if (test == true) {
-        input.nextElementSibling.textContent = "Champ valide";
-    } else {
-        input.nextElementSibling.textContent = "Vous ne pouvez utiliser que des chiffres, lettres, espaces, - et ' ";
-    }
-}
-
-
-// Crée une fonction pour vérifier la validité de l'Email inputs et affiche un message ( succes or error)
-function emailValidity(input) {
-    let emailRegExp = /^[a-z0-9.-_]+[@]{1}[a-z0-9.-_]+[.]{1}[a-z]{2,10}$/gi;
-    test = emailRegExp.test(input.value);
-    if (test === true) {
-        input.nextElementSibling.textContent = "Champ valide";
-    } else {
-        input.nextElementSibling.textContent = "Veuillez saisir une adresse email valide";
-    }
-}
+// Écouter l'événement submit sur le formulaire - Empêcher l'action submit de recharger la page avec la méthode prevendDefault
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    // Création de l'objet contact qui sera envoyé à l'API
+    let contact = {
+        firstName: form.firstName.value,
+        lastName: form.lastName.value,
+        address: form.address.value,
+        city: form.city.value,
+        email: form.email.value,
+    };
+    // Envoi de l'objet contact et du tableau products à l'API
+    fetch("http://localhost:3000/api/products/order", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ contact, products }),
+    })
+        // Obtenir la réponse au format JSON
+        .then((response) => response.json())
+        // Définition de la réponse de l'API en tant que orderDetails et définition de l'action à exécuter
+        .then((orderDetails) => {
+            console.log("L'envoi du formulaire à bien été effectué");
+            // Obtenir l'élément orderId de la réponse de l'API et l'affecter à une variable pour une utilisation ultérieure dans l'url
+            let orderId = orderDetails.orderId;
+            console.log(orderId);
+            if(orderId){
+                // Redirection de l'utilisateur sur la page de confirmation et ajout de orderId dans l'url
+                window.location.href = `./confirmation.html?id=${orderId}`;
+                //Vider la Cart
+                clearCart();
+            }else{
+                alert("Il semble y avoir un problème. Veuillez ré-essayer ultérieurement")
+            }
+        })
+        .catch((error) => {
+            // Affichage d'un message d'erreur si la requête envoyée à l'API n'est pas terminée avec succès
+            console.log("L'envoi du formulaire à l'API a rencontré un problème" + error);
+        });
+});
--- front/js/product.js
+++ front/js/product.js
@@ -1,105 +1,93 @@
-// Récupération de l'id dans l'URL
-let sear= window.location.href
-let url = new URL(sear)
-let idURL = url.searchParams.get("id")
-// Appel API avec l'id du produit
-let productUnit = ""
-let requestURL = "http://localhost:3000/api/products/" + idURL
-fetch(requestURL)
-    .then(response => response.json())
-    .then(async function (resultatAPI) {
-        // await permet d'attendre la résolution d'une promesse (Promise) Lorsque la promesse est résolue (tenue ou rompue),
-        // la valeur est renvoyée et l'exécution de la fonction asynchrone reprend.
-        productUnit = await resultatAPI
-        showProduct(productUnit)
+// Récupération des éléments qui contiendront les détails du produit
+const itemPresentation = document.querySelector(".item");
+const itemContent = document.querySelector("article");
+const itemImgContainer = document.querySelector(".item__img");
+const itemName = document.getElementById("title");
+const itemPrice = document.getElementById("price");
+const itemDescription = document.getElementById("description");
+const itemColor = document.getElementById("colors");
+
+
+// Extraction de l'ID du produit depuis l'URL
+let params = new URL(document.location).searchParams;
+let productId = params.get("id");
+console.log(`Récupération de l'id du produit ayant enregistré le clic sur la page d'accueil : ${productId}`);
+
+
+//envoi d'une requête a l'API avec fetch
+fetch(`http://localhost:3000/api/products/${productId}`)
+    // Si la requête est validé, retourne la réponse en JSON
+    .then((response) => response.json())
+    //définit API response en productDetails
+    .then((productDetails) => {
+        // Insertion de l'image
+        let productImg = document.createElement("img");
+        productImg.setAttribute("src", productDetails.imageUrl);
+        productImg.setAttribute("alt", productDetails.altTxt);
+        itemImgContainer.appendChild(productImg);
+
+        // Insertion du nom
+        let productName = productDetails.name;
+        itemName.textContent = productName;
+
+        // Insertion du prix
+        let productPrice = productDetails.price;
+        itemPrice.textContent = productPrice;
+
+        // Insertion de la description
+        let productDescription = productDetails.description;
+        itemDescription.textContent = productDescription;
+
+        // Insertion des couleurs, parcoure le tableau renvoyé par l'API pour insérer dynamiquement des options de couleur (colorOption) dans l'élément itemColor (.color)
+        let productColors = productDetails.colors;
+        for (i = 0; i < productColors.length; i++) {
+            let colorOption = document.createElement("option");
+            colorOption.setAttribute("value", productColors[i]);
+            colorOption.innerText = productColors[i];
+            itemColor.appendChild(colorOption);
+        }
     })
-    // Ajout un message au cas où le serveur ne répond pas
-    .catch(_error => {
-        alert('Oops ! Le serveur ne répond pas, suivez les instructions dans le READ.me.')
-    })
-// Affichage du produit par page produit
-function showProduct(productData) {
-    document.title = productData.name
-    let panelIMG = document.querySelector('.item__img')
-    let createPict = document.createElement('img')
-    let panelH1 = document.querySelector('#title')
-    let panelPrix = document.querySelector('#price')
-    let panelDescription = document.querySelector('#description')
-// Insertion image du canapé
-    createPict.setAttribute('src', productData.imageUrl)
-    createPict.setAttribute('alt', productData.altTxt)
-    panelIMG.appendChild(createPict)
-// Insertion du nom du canapé
-    panelH1.textContent = productData.name
-// Insertion du prix du canapé
-    panelPrix.textContent = productData.price
-// Insertion du choix du canapé
-    panelDescription.textContent = productData.description
-// Récupération de #colors
-    let panelOption = document.querySelector('#colors')
-// Insertion du tableau des couleurs dans une variable
-    let colors = productData.colors
-// Parcours du tableau de couleurs et insertion de celles-ci dans choix
-    for (let i = 0; i < colors.length; i++){
-        let colorProduct = colors[i]
-        let createOption = document.createElement('option')
-        createOption.setAttribute('value', colorProduct)
-        createOption.textContent = colorProduct
-        panelOption.appendChild(createOption);
-    }
-}
-// Ajouter au localstorage
-// Récupération de #colors, #quantity et #addToCard
-let choixColor = document.querySelector('#colors')
-let choixQuantity = document.querySelector('#quantity')
-let sendToBasket = document.querySelector('#addToCart')
-// Configuration un eventListener quand l'utilisateur clique sur ajouter au panier
-sendToBasket.addEventListener('click', function (event) {
-// Récupération des valeurs de quantité et de couleurs du produit choisi dans des variables
-    let valueColor = choixColor.value
-    let valueQuantity = choixQuantity.value
-    if (valueQuantity <= 0 || valueQuantity > 100 || valueColor == ""){
-        alert("Veuillez choisir une quantité entre 1 et 100 et/ou une couleur de canapé");
+    // si la requête est rejeté, Création d'un message à télécharger dans l'élément item pour informer l'utilisateur que quelque chose s'est mal passé.
+    .catch((error) => {
+        console.log("Il y a eu une erreur dans le chargement du produit sur le site." + error);
+        itemPresentation.removeChild(itemContent);
+        let productErrorMessage = document.createElement("h2");
+        productErrorMessage.textContent = "Nous rencontrons des difficultés techniques pour afficher l'article que vous avez sélectionné. Nos équipes sont à l'oeuvre pour résoudre ce problème dans les plus brefs délais. Nous vous invitons à réessayer ultérieurement et nous excusons pour la gêne occasionnée.";
+        productErrorMessage.style.textAlign = "center";
+        productErrorMessage.style.padding = "15px";
+        itemPresentation.appendChild(productErrorMessage);
+    });
+
+
+
+// Ajout des produits a Cart
+// Récupération du Bouton dans une constante
+const addToCartBtn = document.getElementById("addToCart");
+
+
+// Récupération des quantité et des couleurs dans des variables
+let quantity = document.getElementById("quantity");
+let color = document.getElementById("colors");
+
+// Ajout du listener
+addToCartBtn.addEventListener("click", () => {
+    // Définition des valeurs de colorPicked et de quantityPicked
+    let colorPicked = color.value;
+    let quantityPicked = Number(quantity.value);
+    // Récupération du nom du produit
+    let productName = document.getElementById("title").textContent;
+    // création de l'objet pour cart
+    let product = {
+        id: productId,
+        color: colorPicked,
+        quantity: quantityPicked,
+        name: productName,
+    };
+    // Si la couleur et la quantité ont été choisi, ajoute a Cart sinon envoi un message erreur
+    if (color.value !== "" && quantity.value > 0 && quantity.value <= 100) {
+        addToCart(product);
+        alert("Votre choix a bien été effectué et votre article a été ajouté à votre panier.");
     } else {
-        // Récupération du contenu du panier (sans produit choisi de la page actuel)
-        let basket =  {}
-        let basketStr = localStorage.getItem('basket')
-        if (basketStr == null) {
-            basket = {
-                totalQuantity: 0,
-                products: []
-            }
-        } else {
-            basket = JSON.parse(basketStr)
-        }
-        // Creation du produit choisi
-        let choixProduct = {
-            id: productUnit._id,
-            name: productUnit.name,
-            color: valueColor,
-            quantity: Number(valueQuantity),
-            img: productUnit.imageUrl,
-        }
-        // Ajout de la quantité du produit choisi à la quantité des produits dans le panier (SI ils ont le même id et même color)
-        boolean = false
-        for (let i = 0 ; i < basket.products.length; i++) {
-            basketProduct = basket.products[i]
-            if (basketProduct.id == choixProduct.id && basketProduct.color == choixProduct.color) {
-                newQuantity = basketProduct.quantity + choixProduct.quantity
-                basketProduct.quantity = newQuantity
-                basket.totalQuantity = choixProduct.quantity + basket.totalQuantity
-                boolean = true
-                break
-            }
-        }
-        // Ajout du produit choisi dans le panier (SI ils ont pas le même id et même color)
-        if (boolean == false) {
-            basket.products.push(choixProduct);
-            newQuantity = basket.totalQuantity + choixProduct.quantity
-            basket.totalQuantity = newQuantity
-        }
-        alert(`Votre commande de ${choixProduct.quantity}  ${productUnit.name}  ${choixProduct.color} est bien ajoutée au panier !`)
-        let lineBasket = JSON.stringify(basket)
-        localStorage.setItem("basket", lineBasket)
+        alert("Veuillez sélectionner une couleur et indiquer la quantité souhaitée. Attention, la quantité maximale est fixée à 100 articles.");
     }
-})
+});
